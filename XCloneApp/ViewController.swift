63b08d5cb519baacb9f007814089f0006d3eada7
Manuel E. Hernandez <kicke10h@gmail.com>
2024-11-02 15:21:06 -0600

    [ADD] Git Ignore

---
+++ XCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCloneApp
+//
+//  Created by Kike Hernandez D. on 02/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
