cbb8f8a7d043aab82d5552dc6567afa1c1941b5a
Manuel Hernandez <mehernandezduarte@gmail.com>
2024-11-03 22:54:29 -0600

    [ADD] Complete StartView

--- XCloneApp/Views/StartView/StartViewController.swift
+++ XCloneApp/Views/StartView/StartViewController.swift
@@ -13,6 +13,8 @@ class StartViewController: UIViewController {
     @IBOutlet weak var buttonGoogle: ButtonLogSN!
     @IBOutlet weak var buttonApple: ButtonLogSN!
     @IBOutlet weak var buttonCreateAccount: CustomButton!
+    @IBOutlet weak var termsAndConditionLabel: UILabel!
+    @IBOutlet weak var logInLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +38,83 @@ class StartViewController: UIViewController {
         self.buttonCreateAccount.delegte = self
         
         
+//        CONFIGURE ACTIONS TERMS LABEL
+        let tapGestureTerms = UITapGestureRecognizer(target: self, action: #selector(handleTapTerms(_:)))
+        self.termsAndConditionLabel.addGestureRecognizer(tapGestureTerms)
+        self.termsAndConditionLabel.isUserInteractionEnabled = true
+        
+        let tapGestureLogIn = UITapGestureRecognizer(target: self, action: #selector(handleTapLogIn(_:)))
+        self.logInLabel.addGestureRecognizer(tapGestureLogIn)
+        self.logInLabel.isUserInteractionEnabled = true
+        
     }
+    
+    @objc private func handleTapTerms(_ gesture: UITapGestureRecognizer) {
+        let baseText = self.termsAndConditionLabel.text ?? self.termsAndConditionLabel.attributedText?.string ?? ""
+        
+        let termsRange = (baseText as NSString).range(of: "Terms")
+        let privacyPolicyRange = (baseText as NSString).range(of: "Privacy Policy")
+        let cookieUseRange = (baseText as NSString).range(of: "Cookie Use")
+        
+        let tapLocation = gesture.location(in: self.termsAndConditionLabel)
+        let textBoundingBox = self.termsAndConditionLabel.attributedText?.boundingRect(with: self.termsAndConditionLabel.bounds.size, options: [.usesLineFragmentOrigin, .usesFontLeading], context: nil)
+        
+        guard let boundingBox = textBoundingBox else { return }
+        
+        // Ajusta las áreas de toque según el texto mostrado en el UILabel
+        if tapLocationInRange(tapLocation, range: termsRange, label: self.termsAndConditionLabel) {
+            print("Terms tapped")
+            // Acción para Terms
+        } else if tapLocationInRange(tapLocation, range: privacyPolicyRange, label: self.termsAndConditionLabel) {
+            print("Privacy Policy tapped")
+            // Acción para Privacy Policy
+        } else if tapLocationInRange(tapLocation, range: cookieUseRange, label: self.termsAndConditionLabel) {
+            print("Cookie Use tapped")
+            // Acción para Cookie Use
+        }
+        if tapLocation.x >= boundingBox.minX && tapLocation.x <= boundingBox.maxX && tapLocation.y >= boundingBox.minY && tapLocation.y <= boundingBox.maxY {
+        }
+        
+    }
+    
+    @objc private func handleTapLogIn(_ gesture: UITapGestureRecognizer) {
+        let baseText = self.logInLabel.text ?? self.logInLabel.attributedText?.string ?? ""
+        
+        let logInRange = (baseText as NSString).range(of: "Log in")
+
+        
+        let tapLocation = gesture.location(in: self.logInLabel)
+        let textBoundingBox = self.logInLabel.attributedText?.boundingRect(with: self.logInLabel.bounds.size, options: [.usesLineFragmentOrigin, .usesFontLeading], context: nil)
+        
+        guard let boundingBox = textBoundingBox else { return }
+        
+        // Ajusta las áreas de toque según el texto mostrado en el UILabel
+        if tapLocation.x >= boundingBox.minX && tapLocation.x <= boundingBox.maxX && tapLocation.y >= boundingBox.minY && tapLocation.y <= boundingBox.maxY {
+            if tapLocationInRange(tapLocation, range: logInRange, label: self.logInLabel) {
+                debugPrint("Log In tapped")
+            }
+        }
+        
+    }
+    
+    
+    private func tapLocationInRange(_ location: CGPoint, range: NSRange, label: UILabel) -> Bool {
+            let textStorage = NSTextStorage(attributedString: label.attributedText!)
+            let layoutManager = NSLayoutManager()
+            let textContainer = NSTextContainer(size: label.bounds.size)
+            
+            layoutManager.addTextContainer(textContainer)
+            textStorage.addLayoutManager(layoutManager)
+            
+            textContainer.lineFragmentPadding = 0
+            textContainer.maximumNumberOfLines = label.numberOfLines
+            textContainer.lineBreakMode = label.lineBreakMode
+            
+            let glyphRange = layoutManager.glyphRange(forCharacterRange: range, actualCharacterRange: nil)
+            let boundingRect = layoutManager.boundingRect(forGlyphRange: glyphRange, in: textContainer)
+            
+            return boundingRect.contains(location)
+        }
     
     
     private func getFontNamesByFaily() {
@@ -50,6 +128,8 @@ class StartViewController: UIViewController {
             debugPrint("")
         }
     }
+    
+    
 
 }
 
